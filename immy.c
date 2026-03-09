5fe8de84721fa5aa1f58151fc1d25e4134eef6bf
zacdogwater <zacsengines@gmail.com>
2026-03-08 20:58:55 -0600

    fix ai slop

--- immy.c
+++ immy.c
@@ -14,44 +14,42 @@ int main(int argc, char **argv) {
 	SDL_Window *window = SDL_CreateWindow("immy", 1, 1, SDL_WINDOW_RESIZABLE | SDL_WINDOW_TRANSPARENT);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
 
-	IMG_Animation* anim = IMG_LoadAnimation(argv[1]);
-	SDL_Surface* single_surface = NULL;
-	int frame_count = 1;
-	int* delays = NULL;
-	SDL_Surface** surfaces = NULL;
+	int frames = 1;
+	int *delays = NULL;
+	SDL_Texture **textures = NULL;
 
-	if (anim) {
-		frame_count = anim->count;
-		surfaces = anim->frames;
+	IMG_Animation *anim = IMG_LoadAnimation(argv[1]);
+	if (anim != NULL) {
+		frames = anim->count;
 		delays = anim->delays;
+		textures = malloc(anim->count * sizeof(SDL_Texture *));
+		for (int i = 0; i < frames; i++) {
+			textures[i] = SDL_CreateTextureFromSurface(renderer, anim->frames[i]);
+		}
 	} else {
-		single_surface = IMG_Load(argv[1]);
-		if (single_surface == NULL) {
+		textures = malloc(sizeof(SDL_Texture *));
+		SDL_Surface *surface = IMG_Load(argv[1]);
+		if (surface == NULL) {
 			fprintf(stderr, "Failed to open image '%s': %s\n", argv[1], SDL_GetError());
 			return 1;
 		}
-		surfaces = &single_surface;
+		*textures = SDL_CreateTextureFromSurface(renderer, surface);
+		SDL_DestroySurface(surface);
 	}
- 
-	SDL_Texture** textures = malloc(frame_count * sizeof(SDL_Texture*));
-	for (int i = 0; i < frame_count; i++) {
-		textures[i] = SDL_CreateTextureFromSurface(renderer, surfaces[i]);
-	}
+
 
 	SDL_SetRenderVSync(renderer, 1);
 
 	float w, h;
 	SDL_GetTextureSize(textures[0], &w, &h);
 	SDL_SetWindowSize(window, (int)w, (int)h);
-	SDL_SetWindowShape(window, surfaces[0]);
 
 	SDL_FRect dst_rect = { 0.0f, 0.0f, w, h };
 
 	bool running = true;
-	int current_frame = 0;
-	Uint64 last_time = SDL_GetTicks(); // Track playback time
+	Uint64 last_time = SDL_GetTicks();
 
-	while (running) {
+	for (int frame = 0; running;) {
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
 			switch (event.type) {
@@ -62,17 +60,14 @@ int main(int argc, char **argv) {
 			}
 		}
 
-		// gif handling
-		if (frame_count > 1) {
-			Uint64 current_time = SDL_GetTicks();
-			int delay = delays[current_frame];
-			if (delay <= 0) delay = 100; // fallback
+		if (frames > 1) {
+			Uint64 time = SDL_GetTicks();
+			int delay = delays[frame];
+			if (delay <= 0) delay = 100;
 
-			if (current_time - last_time >= (Uint64)delay) {
-				current_frame = (current_frame + 1) % frame_count;
-				last_time = current_time; 
-				
-				SDL_SetWindowShape(window, surfaces[current_frame]);
+			if (time - last_time >= (Uint64)delay) {
+				frame = (frame + 1) % frames;
+				last_time = time;
 			}
 		}
 
@@ -82,20 +77,15 @@ int main(int argc, char **argv) {
 		SDL_GetWindowSizeInPixels(window, &x, &y);
 		dst_rect.w = (float)x;
 		dst_rect.h = (float)y;
-		SDL_RenderTexture(renderer, textures[current_frame], NULL, &dst_rect);
+		SDL_RenderTexture(renderer, textures[frame], NULL, &dst_rect);
 		SDL_RenderPresent(renderer);
 	}
 
-	for (int i = 0; i < frame_count; i++) {
+	if (anim != NULL) IMG_FreeAnimation(anim);
+	for (int i = 0; i < frames; i++) {
 		SDL_DestroyTexture(textures[i]);
 	}
 	free(textures);
-	
-	if (anim) {
-		IMG_FreeAnimation(anim);
-	} else {
-		SDL_DestroySurface(single_surface);
-	}
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
